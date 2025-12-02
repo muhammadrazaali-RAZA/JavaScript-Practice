bb196ceb0aad0f35e6d19aad8f188d2000588b1f
Muhammad Raza Ali 12248246 <muhammadra98@almighty.cs.univie.ac.at>
2025-12-02 17:56:58 +0100

    Practicing JS, Css, HTML with game ROCK|PAPER|SCISSORS .

--- src/app.js
+++ src/app.js
@@ -2,19 +2,34 @@ let userScore = 0;
 let computerScore = 0;
 
 const choices = document.querySelectorAll('.choice');
+const msg = document.querySelector('#msg');
+const compScoreQry = document.querySelector("#comp-score");
+const userScoreQry = document.querySelector("#user-score");
 
 const drawGame = () => {
-    console.log("Drawing game...");
-}
+    msg.innerText = "Draw game !";
+    msg.style.backgroundColor = "#081b31";
+};
 
 const showWinner = (userWin) => {
-    const winner = userWin? `You Win.` : `You Sucks`;
-    console.log(winner);
-}
+
+    if (userWin) {
+        msg.innerText = `You WIN.`;
+        msg.style.backgroundColor =  "green";
+        userScore++;
+        userScoreQry.innerText = userScore;
+    }else{
+        msg.innerText = `You SUCKS.`;
+        msg.style.backgroundColor =  "red";
+        computerScore++;
+        compScoreQry.innerText = computerScore;
+    }
+};
 
 const playGame = (userChoice) => {
     const compChoice = genComputerChoice();
     let userWin = true;
+
     // Cases
     if (userChoice === compChoice) {
         // Draw game
@@ -23,17 +38,14 @@ const playGame = (userChoice) => {
 
         if (userChoice === "rock") {
             // scissors, paper
-            userWin =  compChoice !== "paper";
-        }else if (userChoice === "paper") {
+            userWin = compChoice !== "paper";
+        } else if (userChoice === "paper") {
             userWin = compChoice !== "scissors";
-        }else{
+        } else {
             userWin = compChoice !== "rock";
         }
-        console.log("User: " + userChoice);
-        console.log("comp: " + compChoice);
-        console.log(userWin);
+        showWinner(userWin);
     }
-    showWinner(userWin);
 };
 
 choices.forEach((choice) => {
@@ -48,4 +60,4 @@ const genComputerChoice = () => {
     let randomNumber =  Math.floor(Math.random() *3);
 
     return choices[randomNumber];
-}
+};
